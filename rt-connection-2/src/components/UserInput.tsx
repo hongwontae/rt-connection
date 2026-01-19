6b5a456f04f037139f25e038f448028250d9c4a8
dnjsxoghd <feelchok1234@gmail.com>
2026-01-19 18:14:54 +0900

    tailwind up

--- rt-connection-2/src/components/UserInput.tsx
+++ rt-connection-2/src/components/UserInput.tsx
@@ -19,49 +19,55 @@ function UserInput() {
     event: React.ChangeEvent<HTMLInputElement>,
     identifier: string,
   ) => {
-
     const value = event.currentTarget.value;
 
-    setData((prev)=>{
-        return {
-            ...prev,
-            [identifier] : identifier == "age" ? Number(value) : value
-        }
-    })
+    setData((prev) => {
+      return {
+        ...prev,
+        [identifier]: identifier == "age" ? Number(value) : value,
+      };
+    });
   };
 
   return (
     <>
-      <div>
-        <label>User Name</label>
-        <input
-          value={data.name}
-          onChange={(e) => handleChange(e, "name")}
-        ></input>
+      <div className="mb-8 flex flex-col gap-4 justify-center items-center">
+        <div className="flex gap-4 items-center">
+          <label className="uppercase">User Name</label>
+          <input
+            className="border rounded-2xl p-1"
+            value={data.name}
+            onChange={(e) => handleChange(e, "name")}
+          ></input>
+        </div>
+        <div className="flex gap-4 items-center">
+          <label className="uppercase">User Age</label>
+          <input
+            className="border rounded-2xl p-1"
+            value={data.age}
+            type="number"
+            onChange={(e) => handleChange(e, "age")}
+          ></input>
+        </div>
+        <div className="flex gap-4 items-center">
+          <label className="uppercase">User Address</label>
+          <input
+            className="border rounded-2xl p-1"
+            value={data.address}
+            onChange={(e) => handleChange(e, "address")}
+          ></input>
+        </div>
+        <div className="flex gap-4 items-center">
+          <label className="uppercase">User Email</label>
+          <input
+            className="border rounded-2xl p-1"
+            value={data.email}
+            onChange={(e) => handleChange(e, "email")}
+          ></input>
+        </div>
       </div>
-      <div>
-        <label>User Age</label>
-        <input
-          value={data.age}
-          type="number"
-          onChange={(e) => handleChange(e, "age")}
-        ></input>
-      </div>
-      <div>
-        <label>User Address</label>
-        <input
-          value={data.address}
-          onChange={(e) => handleChange(e, "address")}
-        ></input>
-      </div>
-      <div>
-        <label>User Email</label>
-        <input
-          value={data.email}
-          onChange={(e) => handleChange(e, "email")}
-        ></input>
-      </div>
-      <div>
+
+      <div className="flex flex-row gap-9">
         <div>{data.name}</div>
         <div>{data.age}</div>
         <div>{data.address}</div>
